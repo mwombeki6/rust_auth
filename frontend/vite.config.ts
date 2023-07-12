169452f0a441c744b2a86e2d5c4d3416b374480c
mwombeki <hackerlubex@protonmail.com>
2023-07-12 10:46:15 +0300

    commit

--- frontend/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import basicSsl from '@vitejs/plugin-basic-ssl';
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [basicSsl(),svelte()],
-})
